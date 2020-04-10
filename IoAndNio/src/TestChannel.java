d2eb9fecff128f32ba4c7702f6e9e9984d7de84a
cyz12 <1093491992@qq.com>
2020-04-10 18:46:02 +0800

    buffer

--- IoAndNio/src/TestChannel.java
+++ IoAndNio/src/TestChannel.java
@@ -146,9 +146,35 @@ public class TestChannel {
 
     @Test
     public void test4(){
-        //RandomAccessFile raf1 = new RandomAccessFile("1.jpeg","rw");
+        FileChannel channel1 = null;
 
-        //ByteBuffer buf1 = ByteBuffer.allocate(100);
-        //ByteBuffer buf2 = ByteBuffer.allocate(1024);
+        try {
+            RandomAccessFile raf1 = new RandomAccessFile("1.txt","rw");
+            channel1 = raf1.getChannel();
+
+            ByteBuffer buf1 = ByteBuffer.allocate(100);
+            ByteBuffer buf2 = ByteBuffer.allocate(1024);
+
+            ByteBuffer[] bufs = {buf1,buf2};
+            channel1.read(bufs);
+
+            for (ByteBuffer byteBuffer : bufs){
+                byteBuffer.flip();
+            }
+
+            System.out.println(new String(bufs[0].array(),0,bufs[0].limit()));
+            System.out.println("----------------------------------");
+            System.out.println(new String(bufs[1].array(),0,bufs[1].limit()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (channel1 != null){
+                try {
+                    channel1.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
