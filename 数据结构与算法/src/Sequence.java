5cbad49b14c3a7f1b0e27cacfa5b8b99193f5a5f
cyz12 <1093491992@qq.com>
2020-05-28 20:58:10 +0800

    归并排序

--- 数据结构与算法/src/Sequence.java
+++ 数据结构与算法/src/Sequence.java
@@ -84,14 +84,110 @@ public class Sequence {
         }
     }
 
+    @Test
+    public void QuickSort(int[] arrays,int L,int R){
+        int i = L;
+        int j = R;
+        int temp;
+        int pivot = arrays[(L+R)/2];
 
+        while (i<=j){
+
+            while (pivot>arrays[i]){
+                i++;
+            }
+            while (pivot<arrays[j]){
+                j--;
+            }
+
+            if (i<=j){
+                temp = arrays[j];
+                arrays[j] = arrays[i];
+                arrays[i] = temp;
+                i++;
+                j--;
+            }
+
+        }
+        if (j>L){
+            QuickSort(arrays,L,j);
+        }
+
+        if (i<R){
+            QuickSort(arrays,i,R);
+        }
+
+
+    }
+
+
+    @Test
+    public void MergeSort(int[] arrays, int L,int R){
+        int i = L;
+        int j = R;
+        int M = (R+L)/2;
+
+        if(L==R){
+            return;
+        }else{
+
+            MergeSort(arrays,L,M);
+
+            MergeSort(arrays,M+1,R);
+
+            merge(arrays,L,M+1,R);
+        }
+
+    }
+
+    public void merge(int[] arrays,int L,int M,int R){
+        int[] leftarrays = new int[M-L];
+        int[] rightarrays = new int[R-M+1];
+
+        for (int i=L;i<M;i++){
+            leftarrays[i-L] = arrays[i];
+        }
+
+        for (int j=R;j<M;j++){
+            leftarrays[j-L] = arrays[j];
+        }
+
+
+        int i = 0,j =0;
+        int k = L;
+        //⽐较这两个数组的值，哪个⼩，就往数组上放
+        while (i < leftarrays.length && j < rightarrays.length) {
+            //谁⽐较⼩，谁将元素放⼊⼤数组中,移动指针，继续⽐较下⼀个
+            if (leftarrays[i] < rightarrays[j]) {
+                arrays[k] = leftarrays[i];
+                i++;
+                k++;
+            } else {
+                arrays[k] = rightarrays[j];
+                j++;
+                k++;
+            }
+        }
+        //如果左边的数组还没⽐较完，右边的数都已经完了，那么将左边的数抄到⼤数组中(剩下的都是⼤数字)
+        while (i < leftarrays.length) {
+            arrays[k] = leftarrays[i];
+            i++;
+            k++;
+        }
+        //如果右边的数组还没⽐较完，左边的数都已经完了，那么将右边的数抄到⼤数组中(剩下的都是⼤数字)
+        while (j < rightarrays.length) {
+            arrays[k] = rightarrays[j];
+            k++;
+            j++;
+        }
+    }
 
     public static void main(String[] args) {
         int[] arr ={3,2,5,4,9,8};
         Sequence sequence = new Sequence();
         //sequence.BubbleSort(arr);
         //sequence.SelectionSort(arr);
-        sequence.InsertionSort(arr);
-
+        //sequence.InsertionSort(arr);
+        //sequence.QuickSort(arr,0,5);
     }
 }
