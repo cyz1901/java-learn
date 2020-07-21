1b3c9d5abea2bab32ad76d149ff209a4b7f29825
cyz12 <1093491992@qq.com>
2020-07-21 22:50:21 +0800

    scala练习

--- Scala函数式编程习题/src/three.scala
+++ Scala函数式编程习题/src/three.scala
@@ -60,13 +60,28 @@ object List { // `List` companion object. Contains functions for creating and wo
       case Cons(_,x) => Cons(h,x)
     }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  def drop[A](l: List[A], n: Int): List[A] =
+    if (n<=0) l
+    else l match {
+      case Nil => Nil
+      case Cons(_,x) => drop(x,n-1)
+    }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+    l match {
+      case Cons(h,t) if f(h) => dropWhile(t, f)
+      case _ => l
+    }
 
-  def init[A](l: List[A]): List[A] = ???
+  def init[A](l: List[A]): List[A] =
+    l match {
+      case Nil => sys.error("init of empty list")
+      case Cons(_,Nil) => Nil
+      case Cons(h,t) => Cons(h,init(t))
+    }
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int =
+    foldRight(l,0)((x,y)=>(y+1))
 
   def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
 
@@ -74,6 +89,8 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   def main(args: Array[String]): Unit = {
 
-    print(setHead(List(1,2,3,4),5))
+    //print(dropWhile(List(1,2,3,4),(x:AnyVal)=> x ==1))
+    //print(init(List(1,2,3,4,5)))
+    print(length(List(1,2,3,4,5)))
   }
 }
